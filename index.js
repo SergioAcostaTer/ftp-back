97dca24792fec8c42f826d5b7cd83cab4eed4da1
Sergio Acosta <sergioacostaquintana@gmail.com>
2023-11-09 20:24:00 +0000

    first test

---
+++ index.js
@@ -0,0 +1,104 @@
+const {
+  listFiles,
+  createFolder,
+  download,
+  connectAndExecute,
+  upload,
+  deleteFile,
+} = require("./services/fileServices");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/to", async (req, res) => {
+  const files = await listFiles("/drive");
+  return res.json(files);
+});
+
+app.get("/to/:path", async (req, res) => {
+  let { path } = req.params;
+  const finalPath = path.split("-").join("/");
+
+  const files = await listFiles(`/drive/${finalPath}`);
+  return res.json(files);
+});
+
+app.post("/mkdir/:path", async (req, res) => {
+  let { path } = req.params;
+  const finalPath = path.split("-").join("/");
+
+  await createFolder(`/drive/${finalPath}`);
+
+  return res.json({ message: "Folder created successfully.", path: finalPath });
+});
+
+app.get("/download/:path", async (req, res) => {
+  const { path } = req.params;
+
+  const finalPath = path.split("-").join("/");
+
+  const writableStream = res;
+
+  try {
+    await download(writableStream, `/drive/${finalPath}`);
+  } catch (error) {
+    console.error("Error downloading file:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+const multiparty = require("multiparty");
+
+app.post("/upload", (req, res) => {
+  const form = new multiparty.Form();
+
+  form.parse(req, async (err, fields, files) => {
+    if (err) {
+      console.error("Error parsing form:", err);
+      return res.status(500).json({ error: "Error parsing form data." });
+    }
+
+    const { remotePath, fileName } = fields;
+    const { file } = files;
+
+    const finalPath = `drive/${remotePath}`.split("-").join("/");
+
+    const uploadPromises = file.map(async (fileInfo) => {
+      const filePath = fileInfo.path;
+      const destinationPath = finalPath + "/" + fileName;
+
+      await upload(filePath, destinationPath);
+    });
+
+    await Promise.all(uploadPromises);
+
+    res.status(200).json({ message: "Upload successful", result: "success" });
+  });
+});
+
+app.delete("/deleteFile/:path", async (req, res) => {
+  const { path } = req.params;
+
+  console.log("path", path);
+
+  const finalPath = `drive/${path.split("-").join("/")}`;
+
+  try {
+    await deleteFile(finalPath);
+    res.status(200).json({ message: "File deleted successfully." });
+  } catch (error) {
+    console.error("Error deleting file:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
---
+++ services/fileServices.js
@@ -0,0 +1,124 @@
+const ftp = require("basic-ftp");
+require("dotenv").config();
+
+async function connectAndExecute(callback) {
+  const ftpClient = new ftp.Client();
+  try {
+    await ftpClient.access({
+      host: process.env.FTP_HOST,
+      user: process.env.FTP_USER,
+      password: process.env.FTP_PASSWORD,
+      secure: process.env.FTP_SECURE === "true",
+    });
+    
+    const result = await callback(ftpClient);
+    return result;
+  } catch (err) {
+    console.error("Error:", err.message);
+    throw err;
+  } finally {
+    ftpClient.close();
+  }
+}
+
+async function listFiles(path) {
+  return connectAndExecute(async (client) => {
+    try {
+      console.log(`Listing files on path: ${path}`);
+      const files = await client.list(path);
+
+      const data = {
+        dir: [],
+        files: [],
+      };
+
+      files.forEach((file) => {
+        if (file.isDirectory) {
+          data.dir.push(file.name);
+        } else {
+          data.files.push(file.name);
+        }
+      });
+
+      return {
+        status: 200,
+        data,
+      };
+    } catch (err) {
+      return {
+        status: 500,
+        data: err,
+      };
+    }
+  });
+}
+
+async function createFolder(path) {
+  return connectAndExecute(async (client) => {
+    try {
+      console.log(`Creating folder on path: ${path}`);
+      const folder = await client.ensureDir(path);
+
+      console.log(`Folder created: ${path}`);
+      return folder;
+    } catch (err) {
+      console.error(`Error creating folder on path ${path}:`, err);
+      throw err;
+    }
+  });
+}
+
+async function download(destination, remotePath, startAt = 0) {
+  return connectAndExecute(async (client) => {
+    try {
+      const size = await client.size(remotePath);
+
+      if (startAt >= size) {
+        throw new Error("Invalid startAt position");
+      }
+
+      const readableStream = await client.downloadTo(
+        destination,
+        remotePath,
+        startAt
+      );
+
+      return readableStream;
+    } catch (err) {
+      throw err;
+    }
+  });
+}
+
+async function upload(path, name) {
+  return connectAndExecute(async (client) => {
+    try {
+      const result = await client.uploadFrom(path, name);
+      return result;
+    } catch (err) {
+      console.error(`Error uploading file to path -> ${name}:`, err);
+      throw err;
+    }
+  });
+}
+
+async function deleteFile(path) {
+  return connectAndExecute(async (client) => {
+    try {
+      const result = await client.remove(path);
+      return result;
+    } catch (err) {
+      console.error(`Error deleting file on path -> ${path}:`, err);
+      throw err;
+    }
+  });
+}
+
+module.exports = {
+  listFiles,
+  createFolder,
+  download,
+  connectAndExecute,
+  upload,
+  deleteFile,
+};
