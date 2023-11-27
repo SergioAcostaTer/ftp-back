4f78bde02efca62b7501de4cca4c417674b89323
Sergio Acosta <sergioacostaquintana@gmail.com>
2023-11-27 19:48:34 +0000

    fetch missing

--- services/cron.js
+++ services/cron.js
@@ -1,17 +1,11 @@
 const cron = require("node-cron");
-const fetch = require("node-fetch");
+const axios = require("axios");
 
 const setUpCron = (resetCallback) => {
   cron.schedule("*/10 * * * *", async () => {
-    try {
-      await fetch("https://ftp-back.onrender.com");
-      console.log("API ping successful");
-    } catch (error) {
-      console.error("Error while pinging API:", error);
-    } finally {
-      const now = resetCallback();
-      console.log(`Total attempts: ${now}`);
-    }
+    const response = await axios.get("https://ftp-back.onrender.com");
+    console.log(response.data);
+    resetCallback();
   });
 };
 
